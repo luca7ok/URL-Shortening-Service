690ccc899879fd409dd563f584abdc59944fdc5f
Tomsa Luca <lucasebastian777@gmail.com>
2025-11-16 20:25:51 +0200

    add ShortURLRequest dto

--- src/main/java/com/controller/ShortURLController.java
+++ src/main/java/com/controller/ShortURLController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import com.domain.ShortURL;
+import com.dto.ShortURLRequest;
 import com.dto.ShortURLResponse;
 import com.service.ShortURLService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,24 +23,28 @@ public class ShortURLController {
         this.shortURLService = shortURLService;
     }
 
-    @GetMapping("/{shortCode}")
-    public ResponseEntity<Void> redirectToLongURl(@PathVariable String shortCode) {
-        String longURL = shortURLService.getLongURL(shortCode);
-
-        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
-                .location(URI.create(longURL))
-                .build();
-    }
-
     @PostMapping("/shorten")
-    public ResponseEntity<ShortURLResponse> createShortURL(@RequestBody String longURL, HttpServletRequest request) {
-        ShortURL shortURL = shortURLService.createShortURL(longURL);
+    public ResponseEntity<ShortURLResponse> createShortURL(@RequestBody ShortURLRequest shortURLRequest, HttpServletRequest request) {
+        ShortURL shortURL = shortURLService.createShortURL(shortURLRequest.longURL());
         String fullShortURL = ServletUriComponentsBuilder.fromContextPath(request)
                 .path("/{shortCode}")
                 .buildAndExpand(shortURL.getShortCode())
                 .toUriString();
 
-        ShortURLResponse response = new ShortURLResponse(longURL, shortURL.getShortCode(), fullShortURL);
+        ShortURLResponse response = new ShortURLResponse(shortURLRequest.longURL(), shortURL.getShortCode(), fullShortURL);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<?> redirectToLongURL(@PathVariable String shortCode) {
+        try {
+            String longURL = shortURLService.getLongURL(shortCode);
+
+            return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+                    .location(URI.create(longURL))
+                    .build();
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
